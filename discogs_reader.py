299a2a90a5419f28b7adb1cdebc641999a1ccefe
Frederic Renner <fred@Frederics-MacBook-Pro.local>
2021-05-16 20:58:35 +0200

    stripped out v0.2

--- discogs_reader.py
+++ discogs_reader.py
@@ -9,27 +9,24 @@ import time
 target_minimum_ratio = 2
 
 # discogs token
-token = "nDPMjQBFGRcgQHkXNZUjBhRlNnMusnodhlTndQZh"
+#token = "nDPMjQBFGRcgQHkXNZUjBhRlNnMusnodhlTndQZh"
 
 # get release_list of html adresses of displayed records on the website
 discogs_search_webpage='https://www.discogs.com/search/?limit=250&sort=want%2Cdesc&ev=em_rs&style_exact=House&style_exact=Tech+House&decade=1990&page=1'
 
-# check if pages exist
+# check if page exist
 if "page" not in discogs_search_webpage:
     discogs_search_webpage += "&page=1"
 
-# how many search pages
-pages_begin = 3
-pages_stop =  5
+# which search pages
+search_pages_begin = 3
+search_pages_stop =  5
 
 release_list=[]
 link_should_contain     = ["release", "master"]
-#link_should_contain     = ["release"]
-#link_should_contain     = ["master"]
 link_should_not_contain = ["search", "seller","add?","sort=have"]   
 
-for page in [1]:
-    
+for page in range(search_pages_begin,search_pages_stop):
 
     link_elements = discogs_search_webpage.split("=")
     link_elements[-1] = str(page) 
@@ -57,6 +54,7 @@ release_list=([item for item, count in collections.Counter(release_list).items()
 for release in release_list:
     print(release)
 
+# get links with have/want ratio
 for rel in release_list:
 
     link_elements = rel.split("/")
@@ -66,65 +64,24 @@ for rel in release_list:
     elif "master" in link_elements:  
         link = "https://www.discogs.com/master/stats/" + release_id
 
-    print(link)
+    #print(link)
     status,response = http.request(link)
     #print(response)
     soup = BeautifulSoup(response, features="html.parser")
     soup.find(string=re.compile("members have this"))
-    #soup.find_all("h3").index("members have this")
     have_want_raw = list(soup.find_all("h3"))
-    #print(type(have_want_raw))
-    #print("members" in str(have_want_raw[3]))
-
     
     for i in have_want_raw:
-        print(type(i))
         stat_str = str(i)
-        print(stat_str)
+        #print(stat_str)
         if "members have this" in stat_str:
             stat_str = stat_str.split(" ")
             have=float(stat_str[stat_str.index("members")-1])
         elif "members want this" in stat_str:
             stat_str = stat_str.split(" ")
             want=float(stat_str[stat_str.index("members")-1])    
-    print(want/have)
+    #print("want/have",want/have)
     
 
-    if want/have >= target_minimum_ratio:
-        #selected_links.append(selected_links)
-    
-        print(link)
-
-""" for rel in release_list:
-    
-    #link = "https://www.discogs.com" + rel 
-
-    #link = "https://api.discogs.com/releases/stats" + rel 
-    status,response = http.request(link)
-    print(response)
-
-    time.sleep(1)
-
-    #https://www.discogs.com/release/stats/80571#collection
-    
-    soup = BeautifulSoup(response, features="html.parser")
-    print(response)
-    have = soup.find_all("a", class_="coll_num")
-    print(have)
-    have = re.search('>(.*)</a>', str(have))
-    
-
-    want = soup.find_all("a", class_="want_num")
-    want = re.search('>(.*)</a>', str(want))
-    have = float(have.group(1))
-    want = float(want.group(1))
-
-    print(have/want)
-
-    if have/want >= target_minimum_ratio:
-        #selected_links.append(selected_links)
-    
-        print(link)
-
-
- """
+    if want/have >= target_minimum_ratio:  
+        print("https://www.discogs.com" + rel)
